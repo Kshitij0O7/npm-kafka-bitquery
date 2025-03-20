369d74b54d17fb3f94866bb783b53d76aa4e872c
Kshitij0O7 <kshitij.mahajan.20042@iitgoa.ac.in>
2025-03-20 18:27:21 +0530

    Single Thread Kafka Streaming Program Implementation

---
+++ index.js
@@ -0,0 +1,67 @@
+const { Kafka } = require('kafkajs');
+const fs = require("fs");
+const { CompressionTypes, CompressionCodecs } = require("kafkajs");
+const LZ4 = require("kafkajs-lz4");
+// require('dotenv').config();
+
+CompressionCodecs[CompressionTypes.LZ4] = new LZ4().codec;
+
+
+const getStream = (_username, _password, _topic) => {
+    // Pre-requisites
+    const username = _username;
+    const password = _password;
+    const topic = _topic;
+
+    const kafka = new Kafka({
+      clientId: username,
+      brokers: [
+        "rpk0.bitquery.io:9093",
+        "rpk1.bitquery.io:9093",
+        "rpk2.bitquery.io:9093",
+      ],
+      ssl: {
+        rejectUnauthorized: false,
+        ca: [fs.readFileSync("server.cer.pem", "utf-8")],
+        key: fs.readFileSync("client.key.pem", "utf-8"),
+        cert: fs.readFileSync("client.cer.pem", "utf-8"),
+      },
+      sasl: {
+        mechanism: "scram-sha-512",
+        username: username,
+        password: password,
+      },
+    });
+    
+    const consumer = kafka.consumer({
+        groupId: username + "-my-group",
+        sessionTimeout: 30000,
+    });
+
+    const run = async () => {
+        await consumer.connect();
+        await consumer.subscribe({ topic, fromBeginning: false });
+    
+        await consumer.run({
+            autoCommit: false,
+            eachMessage: async ({ partition, message }) => {
+                try {
+                    const buffer = message.value;
+                    const logEntry = {
+                      partition,
+                      offset: message.offset,
+                      value: JSON.parse(buffer.toString("utf-8")),
+                    };
+                    console.log(logEntry);
+                } catch (err) {
+                    console.error("Error processing message:", err);
+                }
+            },
+        })
+    };
+    
+    run().catch(console.error);
+}
+
+module.exports = { getStream };
+// getStream("trontest1", "9ijSSnbrj7lldsqq1taSl3YOdujRWB", "tron.broadcasted.transactions");
